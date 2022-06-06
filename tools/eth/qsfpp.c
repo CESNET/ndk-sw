5844d7bfe6e6e5f6c3ab554bf86ac2d926c7ccd9
Martin Spinler <spinler@cesnet.cz>
2022-06-06 15:34:00 +0200

    nfb-eth: [MAINTENANCE] use new I2C *_reg functions

--- tools/eth/qsfpp.c
+++ tools/eth/qsfpp.c
@@ -10,26 +10,41 @@
 #include <err.h>
 #include <stdio.h>
 #include <math.h>
+#include <arpa/inet.h>
 
 #include <nfb/nfb.h>
 #include <netcope/i2c_ctrl.h>
 
 /* I2C registers addresses */
-#define TEMPERATURE   22
-#define RX_IN         34
-#define STXDISABLE    86
-#define V_NAME_FIRST 148
-#define V_NAME_LAST  164
-#define V_PN_FIRST   168
-#define V_PN_LAST    184
-#define COMPLIANCE   131
-#define CONNECTOR    130
-#define REVISION     184
-#define WAVELENGTH   186
-#define WAVELEN_TOL  188
-#define EXT_SPEC_COMPLIANCE     192
+#define SFF8636_IDENTIFIER        0
+#define SFF8636_REV_COMPLIANCE    1
+#define SFF8636_TEMPERATURE      22
+#define SFF8636_RX_INPUT_POWER   34
+#define SFF8636_STXDISABLE       86
+#define SFF8636_CONNECTOR       130
+#define SFF8636_COMPLIANCE      131
+#define SFF8636_VENDOR_NAME     148
+#define SFF8636_VENDOR_PN       168
+#define SFF8636_REVISION        184
+#define SFF8636_WAVELENGTH      186
+#define SFF8636_WAVELENGTH_TOL  188
+#define SFF8636_LINK_CODES      192
+#define SFF8636_VENDOR_SN       196
 
-#define CHANNELS 4
+
+static inline uint16_t qsfp_i2c_read16(struct nc_i2c_ctrl *ctrl, uint8_t reg)
+{
+	uint16_t reg16 = 0;
+
+	/* INFO: Some modules doesn't supports continuous reads, disable burst mode */
+#if 0
+	nc_i2c_read_reg(ctrl, reg, (uint8_t*) &reg16, 2);
+#else
+	nc_i2c_read_reg(ctrl, reg, (uint8_t*) &reg16, 1);
+	nc_i2c_read_reg(ctrl, reg + 1, ((uint8_t*) &reg16) + 1, 1);
+#endif
+	return ntohs(reg16);
+}
 
 /**
  * \brief Print compliance
@@ -57,7 +72,7 @@ void qsfpp_print_compliance(uint32_t reg)
 	}
 }
 
-const char *qsfpp_get_compliance_ext(uint32_t reg)
+const char *sff8024_get_ext_compliance(uint8_t reg)
 {
 	switch (reg) {
 	case 0x00: return "Unspecified";
@@ -93,7 +108,7 @@ const char *qsfpp_get_compliance_ext(uint32_t reg)
  *
  * @param reg Regiter
  */
-const char *qsfpp_get_connector(uint32_t reg)
+const char *sff8024_get_connector(uint8_t reg)
 {
 	switch (reg) {
 	case 0x01: return "SC";
@@ -117,46 +132,59 @@ const char *qsfpp_get_connector(uint32_t reg)
 }
 
 /**
- * \brief Print ASCII text stored in QSFP registers first..last
+ * \brief Print ASCII text stored in QSFP registers
  *
- * @param ifc Interface device
- * @param space Interface space
- * @param sel Channel select
- * @param first First register
- * @param last Last register (not printed)
+ * @param i2c I2C controller
+ * @param reg Register offset
+ * @param count Maximal number of characters
  */
-void qsfpp_print_text(struct nc_i2c_ctrl *i2c, int first, int last)
+
+void qsfp_i2c_text_print(struct nc_i2c_ctrl *i2c, uint8_t reg, unsigned count)
 {
-	int i;
-	uint32_t c;
-	i2c_set_data_bytes(i2c, 1);
-	for (i = first; i < last; i++) {
-		nc_i2c_read(i2c, i, &c);
-		if (c == 0) {
+	/* INFO: Some modules doesn't supports continuous reads, disable burst mode */
+	const unsigned BURST = 1;
+	char text[BURST + 1];
+
+	int ret;
+	unsigned cnt;
+	unsigned i = 0;
+
+	while (count) {
+		cnt = count < BURST ? count : BURST;
+		ret = nc_i2c_read_reg(i2c, reg + i, (uint8_t*)text, cnt);
+
+		if (ret < 0)
 			break;
-		}
-		printf("%c", c);
+		text[ret] = 0;
+
+		printf(text);
+		count -= cnt;
+		i += cnt;
+
+		if (strlen(text) < cnt)
+			break;
 	}
 	printf("\n");
 }
 
 /**
- * \brief Fallback mode - determine plugged transceiver, based on wrong temperature
+ * \brief Fallback mode - determine plugged transceiver by reading first register
  *
- * @param i Interface device
- * @param s Interface space
+ * @param i2c I2C controller
  * @return transceiver is present(plugged) = 1, else 0
  */
 int qsfp_present(struct nc_i2c_ctrl *i2c, int mdev __attribute__((unused)) )
 {
-	uint32_t reg;
-	i2c_set_addr(i2c, 0xA0);
-	i2c_set_data_bytes(i2c, 2);
-	nc_i2c_read(i2c, TEMPERATURE, &reg);
-	if (reg == 0xFFFF)
+	uint8_t reg = 0xFF;
+	/* FIXME: get correct i2c address and check for SFF8636 type! */
+	nc_i2c_set_addr(i2c, 0xA0);
+	nc_i2c_read_reg(i2c, SFF8636_IDENTIFIER, &reg, 1);
+	if (reg == 0xFF)
 		return 0;
 	return 1;
 }
+
+void sff8636_print(struct nc_i2c_ctrl *ctrl);
 
 /**
  * \brief Print informations about transceiver
@@ -166,8 +194,6 @@ int qsfp_present(struct nc_i2c_ctrl *i2c, int mdev __attribute__((unused)) )
  */
 void qsfpp_print(struct nfb_device *dev, int nodeoffset, int node_params)
 {
-	uint8_t i;
-	uint32_t reg;
 	uint32_t i2c_addr;
 
 	struct nc_i2c_ctrl *ctrl;
@@ -192,61 +218,68 @@ void qsfpp_print(struct nfb_device *dev, int nodeoffset, int node_params)
 		return;
 	}
 
-	i2c_set_addr(ctrl, i2c_addr);
+	nc_i2c_set_addr(ctrl, i2c_addr);
 
-	i2c_set_data_bytes(ctrl, 2);
+	sff8636_print(ctrl);
 
-	nc_i2c_read(ctrl, TEMPERATURE, &reg);
-	printf("Temperature                : %.2f C\n", ((float) reg) / 256);
+	nc_i2c_close(ctrl);
+}
+
+void sff8636_print(struct nc_i2c_ctrl *ctrl)
+{
+	const int CHANNELS = 4;
+
+	uint8_t i;
+	uint8_t reg = 0xFF;
+	uint16_t reg16 = 0;
+
+	reg16 = qsfp_i2c_read16(ctrl, SFF8636_TEMPERATURE);
+	printf("Temperature                : %.2f C\n", ((float) reg16) / 256);
 
 	printf("Vendor name                : ");
-	qsfpp_print_text(ctrl, V_NAME_FIRST, V_NAME_LAST);
+	qsfp_i2c_text_print(ctrl, SFF8636_VENDOR_NAME, 16);
 
 	printf("Vendor serial number       : ");
-	qsfpp_print_text(ctrl, 196, 211);
+	qsfp_i2c_text_print(ctrl, SFF8636_VENDOR_SN, 16);
 
 	printf("Vendor PN                  : ");
-	qsfpp_print_text(ctrl, V_PN_FIRST, V_PN_LAST);
+	qsfp_i2c_text_print(ctrl, SFF8636_VENDOR_PN, 16);
 
-	nc_i2c_read(ctrl, COMPLIANCE, &reg);
+	nc_i2c_read_reg(ctrl, SFF8636_COMPLIANCE, &reg, 1);
 	printf("Compliance                 : ");
 	if (reg & 0x80) {
-		nc_i2c_read(ctrl, EXT_SPEC_COMPLIANCE, &reg);
-		printf("%s\n", qsfpp_get_compliance_ext(reg));
+		nc_i2c_read_reg(ctrl, SFF8636_LINK_CODES, &reg, 1);
+		printf("%s\n", sff8024_get_ext_compliance(reg));
 	} else {
 		qsfpp_print_compliance(reg);
 	}
 
-	nc_i2c_read(ctrl, CONNECTOR, &reg);
-	printf("Connector                  : %s\n", qsfpp_get_connector(reg));
+	nc_i2c_read_reg(ctrl, SFF8636_CONNECTOR, &reg, 1);
+	printf("Connector                  : %s\n", sff8024_get_connector(reg));
 
 	printf("Revision                   : ");
-	qsfpp_print_text(ctrl, REVISION, REVISION + 2);
+	qsfp_i2c_text_print(ctrl, SFF8636_REVISION, 2);
 
-	i2c_set_data_bytes(ctrl, 2);
-	nc_i2c_read(ctrl, WAVELENGTH, &reg);
-	printf("Wavelength                 : %.2f nm ", ((float) reg) / 20);
+	reg16 = qsfp_i2c_read16(ctrl, SFF8636_WAVELENGTH);
+	printf("Wavelength                 : %.2f nm ", ((float) reg16) / 20);
 
-	nc_i2c_read(ctrl, WAVELEN_TOL, &reg);
-	printf("+- %.2f nm\n", ((float) reg) / 200);
+	reg16 = qsfp_i2c_read16(ctrl, SFF8636_WAVELENGTH_TOL);
+	printf("+- %.2f nm\n", ((float) reg16) / 200);
 
 	printf("\nRX input power\n");
 	for (i = 0; i < CHANNELS; i++) {
-		nc_i2c_read(ctrl, RX_IN + i * 2, &reg);
+		reg16 = qsfp_i2c_read16(ctrl, SFF8636_RX_INPUT_POWER + i * 2);
 		printf(" * Lane %d                  : %.2f %s (%.2f dBm)\n", i + 1,
-			(reg < 10000) ? ((float)reg) / 10 : ((float) reg) / 10000 ,
-			(reg < 10000) ? "uW" : "mW", 10 * log10(((float) reg) / 10000));
+			(reg16 < 10000) ? ((float)reg16) / 10 : ((float) reg16) / 10000 ,
+			(reg16 < 10000) ? "uW" : "mW", 10 * log10(((float) reg16) / 10000));
 	}
 
-	i2c_set_data_bytes(ctrl, 1);
-	nc_i2c_read(ctrl, STXDISABLE, &reg);
+	nc_i2c_read_reg(ctrl, SFF8636_STXDISABLE, &reg, 1);
 
 	printf("\nSoftware TX disable\n");
 	for (i = 0; i < CHANNELS; i++) {
 		printf(" * Lane %d                  : %sactive\n", i + 1, (reg & (1 << i)) ? "" : "in");
 	}
-
-	nc_i2c_close(ctrl);
 }
 
 /**
@@ -257,7 +290,8 @@ void qsfpp_print(struct nfb_device *dev, int nodeoffset, int node_params)
  */
 void qsfpp_stxdisable(struct nc_i2c_ctrl *ctrl)
 {
-	i2c_set_addr(ctrl, 0xA0);
-	i2c_set_data_bytes(ctrl, 1);
-	nc_i2c_write(ctrl, 86, 0xFF);
+	/* FIXME: get correct i2c address and check for SFF8636 type! */
+	uint8_t reg = 0xFF;
+	nc_i2c_set_addr(ctrl, 0xA0);
+	nc_i2c_write_reg(ctrl, SFF8636_STXDISABLE, &reg, 1);
 }
