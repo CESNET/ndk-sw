31c377fc91b21f96e89fb2baa1834b840291fcb9
Martin Spinler <spinler@cesnet.cz>
2022-09-02 01:58:28 -0400

    tool info: [FEATURE] show project version

--- tools/info/info.c
+++ tools/info/info.c
@@ -257,6 +257,10 @@ void print_common_info(struct nfb_device *dev, int verbose)
 	if (len > 0)
 		printf("Project variant            : %s\n", (const char *)prop);
 
+	prop = fdt_getprop(fdt, fdt_offset, "project-version", &len);
+	if (len > 0)
+		printf("Project version            : %s\n", (const char *)prop);
+
 	prop32 = fdt_getprop(fdt, fdt_offset, "build-time", &len);
 	if (len == sizeof(*prop32)) {
 		build_time = fdt32_to_cpu(*prop32);
