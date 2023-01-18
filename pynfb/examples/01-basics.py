cf37a28f7d00857754780949532929f4c52850ac
Martin Spinler <spinler@cesnet.cz>
2023-01-18 14:36:27 +0100

    pynfb: [DOC] add examples

---
+++ pynfb/examples/01-basics.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3
+
+####################################
+# Python NFB API examples - basics #
+####################################
+
+# TOC
+#####
+
+# 1.A Basic device manipulation
+# 1.B Device Tree
+# 1.C Component manipulation
+
+
+# 1.A Basic device manipulation
+##############################
+
+import nfb
+
+# get main device handle
+dev = nfb.open()
+dev = nfb.open("0")
+dev = nfb.open("/dev/nfb0")
+
+
+# 1.B Device Tree
+################
+
+# for DT access is used external fdt module: https://pypi.org/project/fdt/
+fdt = dev.fdt
+print(fdt.info())
+
+# get all nodes for specific compatible string
+nodes = dev.fdt_get_compatible("netcope,eth")
+
+# access to value of node property
+phandle = nodes[0].get_property("rxmac").value
+assert isinstance(phandle, int)
+
+node = dev.fdt_get_phandle(phandle)
+
+# show full path of node in DT
+print(node.path + '/' + node.name)
+
+
+# 1.C Component manipulation
+###########################
+
+# get component handle
+
+# use fdt node directly
+comp = dev.comp_open(node)
+# use compatible string, default index of compatible component in DT is 0
+comp = dev.comp_open("netcope,idcomp")
+# use specific index of compatible component in DT
+comp = dev.comp_open("netcope,idcomp", 0)
+
+
+# write access
+# ------------
+
+# write 16b integer value into component register on offset 0
+# variants: write8, write16, write32, write64
+comp.write16(0, 0x1234)
+
+# write 3 bytes into component register on offset 1
+# on 32b MI bus it realizes one write cycle to
+# address 0 with BE=int('1110', 2)
+comp.write(1, bytes([0, 1, 2]))
+
+
+# read access
+# -----------
+
+# read 32b integer value: ret = 0xdeadcafe
+# variants: read8, read16, read32, read64
+print(comp.read32(0))
+
+# read 3 bytes on offset 18: ret = bytes([0xff, 0xff, 0xff])
+# on 32b MI bus it realizes two consecutive read cycles to
+# address 16 with BE=int('1100', 2) and
+# address 20 with BE=int('0001', 2)
+val = comp.read(0x12, 3)
---
+++ pynfb/examples/02-data-transfer.py
@@ -0,0 +1,135 @@
+#!/usr/bin/python3
+
+###########################################
+# Python NFB API examples - data transfer #
+###########################################
+
+# TOC
+#####
+
+# 2.A Data transmission on specific queue
+# 2.B Data transmission on multiple queues
+# 2.C Queues statistics
+
+import nfb
+dev = nfb.open()
+for e in dev.eth:
+    e.enable()
+    e.pma_local_loopback = True
+
+# 2.A Data transmission on specific queue
+#########################################
+
+# NFB data plane representer
+ndp = dev.ndp
+rxq = ndp.rx[0]
+txq = ndp.tx[0]
+
+# start the queue; this step is optional
+# for loopback test purposes helps to avoid discarding on RxQ)
+rxq.start()
+txq.start()
+
+# Raw frames are represented by bytes
+pkt = bytes([0]*64)
+hdr = bytes([0]*16)
+
+
+# Transmitting frames
+# -------------------
+
+# send 64B frame
+txq.send(pkt)
+# write two 64B frames into buffer
+txq.send([pkt] * 2, flush=False)
+# send the pending frames
+txq.flush()
+
+# send two 64B frame with the same 16B header
+txq.send([pkt] * 2, hdrs=hdr)
+# send two 64B frame, one with 16B header, one with 32B header
+txq.send([pkt] * 2, hdrs=[hdr, hdr + hdr], flags=0)
+
+# send a frame from prepared tuple
+msg = (pkt, hdr, 0)
+txq.sendmsg([msg])
+
+
+# Receiving frames
+# ----------------
+
+# receive all pending frames
+pkts = rxq.recv()
+assert len(pkts) == 0 or isinstance(pkts[0], bytes)
+
+# receive all pending frames
+# if no frame is available, wait for incoming frames maximally 0.5 sec
+pkts = rxq.recv(timeout=0.5)
+
+# try to receive specific count of frames
+# returned list can be smaller, if there is not enough pending frames
+pkts = rxq.recv(cnt=1)
+
+# try to receive specific count of frames
+# if not enough frames is available, wait for incoming frames maximally 2 secs
+pkts = rxq.recv(cnt=1, timeout=2)
+
+# receive specific count of frames, wait indefinitely
+#rxq.recv(cnt=1, timeout=None)
+
+# receive list of messages
+# message is a tuple(pkt: bytes, hdr: bytes, flags: int)
+msgs = rxq.recvmsg()
+assert len(msgs) == 0 or (isinstance(msgs[0], tuple) and len(msgs[0]) == 3)
+
+
+# 2.B Data transmission on multiple queues
+##########################################
+
+# Transmitting frames
+# -------------------
+
+# send 64B frame to all queues
+ndp.send(pkt)
+# send 64B frame to first three queues
+ndp.send(pkt, i=[0, 1, 2])
+# write 64B frame into buffer of all queues
+ndp.send(pkt, flush=False)
+# send pending frames on all queues
+ndp.flush()
+
+# send a frame from prepared tuple on all queues
+ndp.sendmsg(msg)
+
+
+# Receiving frames
+# ----------------
+
+# receive pending frames from all queues
+# return value is list of tuple(pkt: bytes, queue_index: int)
+pkts = ndp.recv()
+assert len(pkts) == 0 or (isinstance(pkts[0], tuple) and len(pkts[0]) == 2 and isinstance(pkts[0][0], bytes))
+
+# receive pending frames on specific queue
+ndp.recv(i=1)
+# receive pending frames on first three queues
+ndp.recv(i=[0, 1, 2])
+# receive specific count of frames from any queues, wait indefinitely
+#ndp.recv(timeout=None)
+
+# receive pending messages from all queues, return value is list of tuple(tuple(pkt: bytes, hdr: bytes, flags: int), queue_index: int)
+msgs = rxq.recvmsg()
+assert len(msgs) == 0 or (isinstance(msgs[0], tuple) and len(msgs[0]) == 2 and isinstance(msgs[0][0][0], bytes))
+
+
+# 2.C Queues statistics
+#######################
+
+try:
+    # stats == {'packets': 4, 'bytes': 276}
+    stats = txq.stats_read()
+
+    # Reset Queue statistics
+    txq.stats_reset()
+except:
+    pass
