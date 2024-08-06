b056a42dcb084739b22e59ca05f126215a6bb430
Martin Spinler <spinler@cesnet.cz>
2024-08-06 12:44:26 +0200

    pynfb: [FIX] add kernel headers for pynfb build

--- pynfb/setup.py
+++ pynfb/setup.py
@@ -1,9 +1,24 @@
 from setuptools import Extension, setup
 from Cython.Build import cythonize
 
+import pathlib
+
 # This setup.py uses the system default path for includes/libraries,
 # so the nfb-framework package must be already installed.
 # If isn't installed, define include_dirs and library_dirs as in setup.py.in
+ext_extra_args = dict(
+    include_dirs=[
+        str(pathlib.Path(__file__).parent / pathlib.Path("../libnfb/include")),
+        str(pathlib.Path(__file__).parent / pathlib.Path("../drivers/kernel/include")),
+    ],
+    library_dirs=[
+        str(pathlib.Path(__file__).parent / pathlib.Path("../cmake-build/libnfb")),
+    ],
+)
+
+libnfb_ext_extra_args = libnetcope_ext_extra_args = dict()
+#libnfb_ext_extra_args = libnetcope_ext_extra_args = ext_extra_args
+
 
 setup(
     name = "nfb",
@@ -13,14 +28,8 @@ setup(
     ext_package = "nfb",
     ext_modules = cythonize(
         [
-            Extension("libnfb", ["nfb/libnfb.pyx"], libraries=["nfb", "fdt"],
-#                include_dirs=[str(pathlib.Path(__file__).parent / pathlib.Path("../libnfb/include"))],
-#                library_dirs=[str(pathlib.Path(__file__).parent / pathlib.Path("../cmake-build/libnfb"))],
-            ),
-            Extension("libnetcope", ["nfb/libnetcope.pyx"], libraries=["nfb", "fdt"],
-#                include_dirs=[str(pathlib.Path(__file__).parent / pathlib.Path("../libnfb/include"))],
-#                library_dirs=[str(pathlib.Path(__file__).parent / pathlib.Path("../cmake-build/libnfb"))],
-            ),
+            Extension("libnfb", ["nfb/libnfb.pyx"], libraries=["nfb", "fdt"], **libnfb_ext_extra_args),
+            Extension("libnetcope", ["nfb/libnetcope.pyx"], libraries=["nfb", "fdt"], **libnetcope_ext_extra_args),
         ],
         include_path=['nfb'],
         compiler_directives={'embedsignature': True, 'binding': False},
