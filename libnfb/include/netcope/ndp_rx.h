ec81e28304c6b6155a3a6bafbf48629131c32dfa
Vladislav Valek <xvalek14@vutbr.cz>
2025-01-22 15:45:47 +0100

    fix(libnetcope-ndp): mask DMA hdr metadata to obtain APP hdr size
    The app header travels with the data and is also stored in the data
    buffer and its size can be retrieved by reading and masing of DMA hdr
    metadata.

--- libnfb/include/netcope/ndp_rx.h
+++ libnfb/include/netcope/ndp_rx.h
@@ -336,7 +336,7 @@ static inline unsigned nc_ndp_v3_rx_burst_get(void *priv, struct ndp_packet *pac
 			break;
 		}
 
-		header_size = hdr->metadata & NDP_CALYPTE_METADATA_HDR_SIZE_MASK;
+		header_size = le16_to_cpu(hdr->metadata) & NDP_CALYPTE_METADATA_HDR_SIZE_MASK;
 		packet_size = le16_to_cpu(hdr->frame_len) - header_size;
 
 		data = data_base + hdr->frame_ptr * NDP_RX_CALYPTE_BLOCK_SIZE;
