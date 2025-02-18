ca2ae6dcdc7543caee13f4e358aca0c36ce52934
Martin Spinler <spinler@cesnet.cz>
2025-02-18 07:49:54 +0100

    fix(NDP / DMA Calypte): open TX buffer components in kernel space too

--- libnfb/include/netcope/ndp_calypte.h
+++ libnfb/include/netcope/ndp_calypte.h
@@ -11,7 +11,6 @@
 // opened using this function.
 static inline int ndp_queue_calypte_open_buffers(struct nfb_device *dev, struct nc_ndp_queue *q, const void *fdt, int fdt_offset)
 {
-#ifndef __KERNEL__
 	int ctrl_node_offset = -1;
 	int buffer_offset = -1;
 
@@ -46,19 +45,16 @@ err_hdr_buff_open_tx:
 err_hdr_buff_find_tx:
 	nfb_comp_close(q->u.v3.tx_data_buff);
 	return -EBADFD;
-#endif
 	return 0;
 }
 
 static inline void ndp_queue_calypte_close_buffers(struct nc_ndp_queue *q)
 {
-#ifndef __KERNEL__
 	if (q->protocol == 3 && q->channel.type == NDP_CHANNEL_TYPE_TX) {
 		nfb_comp_close(q->u.v3.tx_data_buff);
 		nfb_comp_close(q->u.v3.tx_hdr_buff);
 		q->u.v3.tx_data_buff = NULL;
 		q->u.v3.tx_hdr_buff = NULL;
 	}
-#endif
 	return;
 }
